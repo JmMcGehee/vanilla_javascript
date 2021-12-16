c675c8e15063cb645f811df400cf6c52c64741c3
Joshua McGehee <mcgehee.joshua@gmail.com>
2021-12-16 09:32:36 -0700

    started js crash course

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # vanilla_javascript
 js basics
+
+## A free course from Traversy Media. 
+
+[YouTube Playlist](https://www.youtube.com/watch?v=hdI2bqOjy3c&list=PLillGF-RfqbbnEGy3ROiLWk7JMCuSyQtX)
+
+|Video|runtime|
+|-------------------------|----------|
+|[JavaScript Crash Course For Beginners](https://youtu.be/hdI2bqOjy3c)| 1h 40min 29sec|
+
+### Resources
+
+[Mozilla Developer Network: Console Methods](https://developer.mozilla.org/en-US/docs/Web/API/console#methods)
---
+++ crash_course_for_beginners/main.js
@@ -0,0 +1,38 @@
+// console.log("Say \"what\" again!");
+
+// let age = 30
+// const age = 30 // Throws an error because 'const' can't be reassigned.
+// age = 31
+
+// console.log(age)
+
+// ======= DATA TYPES: String, Number, Boolean, null, undefidined. ======
+
+const name = 'John'
+const age = 30
+const rating = 4.5
+const isCool = true
+const x = null // null is an object??
+const y = undefined
+let z 
+
+// console.log(typeof x);
+
+// ========= Concatenation =========
+// console.log("My name is " + name + " and I am " + age);
+// ======= Template Strings ========
+// console.log(`My name is ${name} and I am ${age}`);
+
+const s = 'Hello World'
+const l = 'tech, monkey, sweet, code'
+console.log(s.length); // .length is a property, not a method. Hence no ()
+// console.log(s.toLocaleUpperCase());
+// console.log(s.substring(0,5).toLocaleUpperCase());
+console.log(s.split('')); // Creates an array of each letter
+console.log(l.split(', '));
+
+// ======================= Arrays ==========================
+
+const numbers = new Array(1)
+
+// Paused at 25 min. 
